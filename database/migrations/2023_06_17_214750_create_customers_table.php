2d12f9cf33c8744c5c0b45f67a42e1d8be5547a6
Ravan Osmanli <ravan.osmanli@ufaz.az>
2023-06-18 01:55:06 +0400

    wip

--- database/migrations/2023_06_17_214750_create_customers_table.php
+++ database/migrations/2023_06_17_214750_create_customers_table.php
@@ -9,17 +9,21 @@ class AddColumnsToCustomersTable extends Migration
     public function up()
     {
         Schema::table('customers', function (Blueprint $table) {
+            $table->string('id')->nullable();
+            $table->string('name')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->date('date')->nullable();
             $table->string('area')->nullable();
             $table->string('price')->nullable();
             $table->string('payment_type')->nullable();
+            $table->timestamps();
         });
     }
 
     public function down()
     {
         Schema::table('customers', function (Blueprint $table) {
-            $table->dropColumn(['area', 'price', 'payment_type']);
+            $table->dropColumn(['id', 'name', 'phone_number', 'date', 'area', 'price', 'payment_type', 'created_at', 'updated_at']);
         });
     }
 }
-
