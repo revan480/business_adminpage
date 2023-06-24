17535b9d91cd9e3dac10fd0a69e6c55dd86eb400
Ravan Osmanli <ravan.osmanli@ufaz.az>
2023-06-25 03:45:11 +0400

    wip

--- resources/views/whatsapp.blade.php
+++ resources/views/whatsapp.blade.php
@@ -38,7 +38,7 @@
                             @if ($customer->isMessageSent)
                                 <span class="text-success">Mesaj Göndərildi</span>
                             @else
-                                <button class="btn btn-primary send-message-btn" data-phone="{{ $customer->phone_number }}">Whatsapp Mesaj Göndər</button>
+                                <a href="https://wa.me/994{{ $customer->phone_number }}" target="_blank" class="btn btn-primary send-message-btn">Whatsapp Mesaj Göndər</a>
                             @endif
                         </td>
                     </tr>
@@ -51,48 +51,17 @@
         </div>
     </div>
 
-    <!-- Confirmation Modal -->
-    <div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-labelledby="confirmationModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="confirmationModalLabel">Təsdiq Mesajı</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <p>Whatsapp mesaji göndərməyə əminsiniz?</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">İmtina Et</button>
-                    <button type="button" class="btn btn-primary" id="confirmSend">Mesaj Göndər</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
+    <!-- Bootstrap and jQuery JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-
     <script>
-        $(document).ready(function() {
-            $('.send-message-btn').click(function() {
-                let row = $(this).closest('tr');
-                let phoneNumber = $(this).data('phone');
-                $('#confirmSend').data('phoneNumber', phoneNumber);
-                $('#confirmSend').data('row', row);
-                $('#confirmationModal').modal('show');
-            });
-
-            $('#confirmSend').click(function() {
-                let phoneNumber = $(this).data('phoneNumber');
-                let row = $(this).data('row');
-                $('#confirmationModal').modal('hide');
-                window.open(`https://wa.me/994${phoneNumber}`, '_blank');
-                row.find('td:last').html('<span class="text-success">Message Sent</span>');
-            });
+        // Send WhatsApp message when the button is clicked and update the button text to "Message Sent" and disable it
+        $('.send-message-btn').click(function() {
+            $(this).text('Mesaj Göndərildi').addClass('disabled');
         });
+
+        
+        
+
     </script>
 @endsection
